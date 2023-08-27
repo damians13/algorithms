3c598796801a3966777bb3d43fa25c9cd9c4056d
Damian Suidgeest <damiansuidgeest@gmail.com>
2023-08-26 18:14:25 -0700

    Create Prim page using BFS as a base

--- src/pages/Prim.jsx
+++ src/pages/Prim.jsx
@@ -1,3 +1,7 @@
+import "../styles/Prim.css"
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter"
+import java from "react-syntax-highlighter/dist/cjs/languages/prism/java"
+import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import { generateWeightedAdjacencyLists, generateWeightedConnectedGraph } from "../components/Graph"
 import MinHeap from "../components/MinHeap"
 
@@ -65,7 +69,66 @@ export class PrimStateObject {
 }
 
 function Prim() {
-	return <p>Prim's algorithm</p>
+	SyntaxHighlighter.registerLanguage("java", java)
+
+	function handleGoClick() {}
+	function handleRandomizeClick() {}
+	function generateAdjacencyListElements() {}
+
+	return (
+		<div className="page">
+			<div id="prim-box" className="fg-box">
+				<p className="title-text">Prim's algorithm</p>
+				<p className="title-description">minimum spanning tree of a weighted graph</p>
+				<div id="prim-content">
+					<div id="prim-boxes">
+						<canvas id="prim-edge-canvas"></canvas>
+					</div>
+					<div id="prim-buttons">
+						<label htmlFor="prim-start-number">Start from</label>
+						<input type="number" min="1" max="12" name="prim-start-number" id="prim-start-num" />
+						<button className="button prim-button" onClick={handleGoClick}>
+							GO
+						</button>
+						<button className="button prim-button" onClick={handleRandomizeClick}>
+							RANDOMIZE
+						</button>
+					</div>
+					<div id="prim-adjacency-list">
+						<p id="prim-adj-list-label">Adjacency lists:</p>
+						{generateAdjacencyListElements()}
+					</div>
+				</div>
+			</div>
+			<div id="bfs-extra" className="extra">
+				<div className="fg-box">
+					<p className="extra-box-text">Steps</p>
+					<div className="extra-box-children">
+						<ol type="1">
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci sunt optio labore non, commodi deleniti quae sequi in ipsam nemo sapiente
+								repellendus ratione mollitia reprehenderit voluptatem illum eligendi rem saepe!
+							</li>
+							<li>
+								Labore placeat rem aspernatur voluptates minus soluta, cupiditate non animi dolor delectus deleniti a accusamus voluptate nostrum dolores sed?
+								Corporis cupiditate accusantium fugiat quod facere similique suscipit. Dignissimos, dolorem dolores.
+							</li>
+							<li>
+								Ea error, incidunt esse tempore expedita nihil alias numquam quibusdam eaque eligendi sed nam iste perspiciatis dicta veniam commodi nobis.
+								Laudantium, hic voluptates! Aspernatur modi itaque veniam tenetur, vel sequi!
+							</li>
+						</ol>
+					</div>
+				</div>
+				<div className="fg-box">
+					<p className="extra-box-text">Code</p>
+					<div className="extra-box-children">
+						<SyntaxHighlighter language="java" style={tomorrow} showLineNumbers>{`// example code here`}</SyntaxHighlighter>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Prim
